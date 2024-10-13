76090bbe774efb8d92f85967ba51c6c0fb969587
John Samarasinghe <john.samarasinghe@outlook.com>
2024-10-13 23:25:05 +0530

    Simulator

--- LayoutModels/Simulator.cs
+++ LayoutModels/Simulator.cs
@@ -3,9 +3,11 @@ using LayoutModels.Support;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace LayoutModels
 {
@@ -14,11 +16,29 @@ namespace LayoutModels
         public event EventHandler<string>? Response;
 
         public Translator CommSpec { get; set; }
+        // public Dictionary<string, Station> Stations { get; set; }
+        // public Dictionary<string, Manipulator> Manipulators { get; set; }
 
-        public Simulator(CommandStructure commStruct, ICommSpec commSpec)
+        public Simulator(CommandStructure commStruct, ICommSpec commSpec, string xmlPath)
         {
             CommSpec = new Translator(commStruct, commSpec);
             CommSpec.Log += Log;
+
+            XmlDocument simDoc = new XmlDocument();
+            simDoc.Load(System.IO.Path.GetFullPath(xmlPath));
+            
+            foreach(XmlNode node in simDoc.GetElementsByTagName("Station"))
+            {
+                for (int i = 0; i < (int)node.SelectSingleNode("Count"); i++)
+                {
+
+                }
+            }
+            foreach (XmlNode node in simDoc.GetElementsByTagName("Manipulator"))
+            {
+                Console.WriteLine(node.InnerText);
+            }
+
         }
 
         public void ProcessCommands(string command)
--- LayoutSimulatorConsole/Program.cs
+++ LayoutSimulatorConsole/Program.cs
@@ -3,7 +3,7 @@ using LayoutModels;
 
 string? command = null;
 
-Simulator simulator = new Simulator(new CommandStructure(false, null, null, ",", 0, 1, 2, null, 3), new UniversalCommSpec());
+Simulator simulator = new Simulator(new CommandStructure(false, null, null, ",", 0, 1, 2, null, 3), new UniversalCommSpec(), "simulation1.xml");
 
 
 
